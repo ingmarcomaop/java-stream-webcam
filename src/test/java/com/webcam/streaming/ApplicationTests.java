603653c6f3d445c53aec2fa70b6a981e0fc5e04b
marcoquind <marco.ortega@quind.io>
2020-03-25 23:53:08 -0500

    First configuration and html front end added

---
+++ src/test/java/com/webcam/streaming/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcam.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
